af517fca717fc7b47004297999e12ca5117569b6
Sindhu S <sindhu@bytebeam.io>
2023-01-09 20:57:13 +0530

    Add explanation for cocktail_recipes.clj

--- src/clj/cocktail_recipes.clj
+++ src/clj/cocktail_recipes.clj
@@ -27,8 +27,7 @@
   (client/get (str filter-url ingredient) {:as :json}))
 
 (get-recipes (first ingredients))
-;; Expand the above to see the result of the HTTP call. It has a lot of things we are not interested in.
-;; Clojure has nifty in-built methods. `first` gives you first item in a collection.
+;; Expand the above to see the result of the HTTP call. It has a lot of things we are not interested in. Fornuately, Clojure has nifty in-built methods. `first` gives you first item in a collection.
 
 ;; Let's make this more precise
 (get-in (get-recipes (first ingredients)) [:body :drinks])
@@ -41,8 +40,12 @@
 
 (ImageIO/read (File. "./src/clj/img/gotta_pay.png"))
 
+;; We can get around this with help of Clojure. Let's us see how
+
 (defn add-name [ingredient]
-(fn [drinks] {(keyword ingredient) drinks}))
+  (fn [drinks] {(keyword ingredient) drinks}))
+
+;; Let us write a better implementation of `get-recipes`
 
 (defn get-recipes-v2
   "Searches for cocktail recipes given ingredient"
@@ -52,7 +55,16 @@
    (get-in [:body :drinks])
    ((add-name ingredient))))
 
+;; You may see strange new syntax here. The ➡️ is a threading macro that takes output of an expression and inserts it as first input of next expressions. 
+
+;; At this point, I encourage you to google terms that you are not familiar with. However, you needn't worry about them too much on what exactly these features are or how they work under the hood. Treat them like a black box for the time-being.
+
+;; `get-in` allows you to extract values in nested maps
+;; Notice that the last expression has _extra_ parenthesis around them, that's because we want the output of `add-name` function.
+
 (def recipes (apply merge (map get-recipes-v2 ingredients)))
+
+;; What's going on here? `map` returns a list of maps and because `merge` takes individual maps, we use `apply` to _apply_ merge on all of them.
 
 (defn flatten-ingredients [[ingredient drinks]]
   (map (fn [drink] (assoc drink :ingredient ingredient)) drinks))
@@ -62,14 +74,11 @@
 (def drink-ids (group-by :idDrink with-ingredients))
 
 (defn summarize [[id drinks]]
-  {
-   :idDrink id 
+  {:idDrink id
    :strDrink (:strDrink (first drinks))
    :strDrinkThumb (:strDrinkThumb (first drinks))
    :count (count drinks)
-   :ingredients (map :ingredient drinks)
-   }
-  )
+   :ingredients (map :ingredient drinks)})
 
 (def drink-summary (map summarize drink-ids))
 
@@ -79,35 +88,35 @@
 ;; unfortunately nothing that uses 3 or more.
 
 (def possible-drinks (filter #(= (:count %) 2) drink-summary))
+;; here we filter recipes which use 2 ingredients
 
 (defn get-thumbnail [drink]
   (ImageIO/read (URL. (:strDrinkThumb drink))))
 
 (def recipe-url "http://thecocktaildb.com/api/json/v1/1/lookup.php?i=")
 
-(defn get-recipe-instructions [drink-id]
-  (->
-   (client/get (str recipe-url drink-id) {:as :json})
-   (get-in [:body :drinks 0 :strInstructions])))
-
 {:nextjournal.clerk/visibility {:code :hide :result :hide}}
+
 (require '[nextjournal.clerk :as clerk])
 
 (defn caption [text url]
   (clerk/html
    [:a {:href url}
-    [:span.text-slate-500.text-xs.text-center.font-sans text]
-    ]
-   ))
+    [:span.text-slate-500.text-xs.text-center.font-sans text]]))
 
-(defn styled-image [title url image ]
-  (clerk/col image (caption title url)))
+(defn styled-image [title url image]
+  (clerk/col {::clerk/opts {:width 100}} image (caption title url)))
 
 (def images-col (map #(-> %
                           (get-thumbnail)
                           ((partial styled-image (:strDrink %)
                                     (str recipe-url (:idDrink %)))))
                      possible-drinks))
+
 {:nextjournal.clerk/visibility {:code :hide :result :show}}
 
+;; # Ta-da!
+;; These are the recipes possible with exactly 2 ingredients
 (apply clerk/row images-col)
+
+;; Clicking on the title of the drink will take you to the recipe API page

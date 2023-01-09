ef6a4110dbadd97068c1c4e2c2abd9de780f95b2
Sindhu S <sindhu@bytebeam.io>
2023-01-09 20:19:16 +0530

    Add first 2 chapters Update HTML

--- src/clj/hello_world.clj
+++ src/clj/hello_world.clj
@@ -5,11 +5,7 @@
 (ns clj.hello-world)
 ;; This is a declaration of the namespace. Namespace allows your variables to stay contained within them. 
 
-;; Note that we use *kebab-case* for names in clojure. But for filenames we use *snake_case*.
-;; `clj/hello_world.clj` has a namespace of `clj.hello-world`. Notice slashes became dots
-;; and underscores became dashes.
-
-;; ### How to create a symbol?
+;; ### How to create a variable?
 (def say "hello ")
 ;; In clojure we don't have variables like in other languages. We have symbols.
 ;; Symbols are like variables, but they are immutable. You can't change the value of a symbol.
@@ -22,50 +18,16 @@
 ;; changed at lexical stage. (colloquially compile time)
 ;; But this practise is discouraged in clojure. You don't want to rebind, because it is confusing.
 
-;; Sometimes we have a symbol which is a constant. This will cause clojure to directly
-;; inline the value of the symbol in the compiled code.
-(def ^:const pi 3.14159)
-;; This `^:const` is called metadata. You can add as many metadata as you want to a symbol.
-
-(:const (meta #'pi))
-
-(:const (meta #'say))
-
-;; metadatas are key value pairs, in this case :const is the key and true is the value.
-;; We use this often to add documentation to our code.
-
-;; ### How do we create a variable? 
-;; This is complicated, and we usually don't need it. We can usually get away with writing all our state into a database. 
-;; Clojure has different types of variables which we will cover in a later chapter.
-
 ;; ### How to define a function?
 (defn greet 
   "Creates a greeting"
   [name]
   (str say name))
 
-;; Notice that you can add inline function documentation after the name of the function. Note
-;; this function does not print anything. It just returns a string. Such functions are called
-;; pure functions because they don't have any effect on the world, also called side effect free.
+;; Notice that you can add inline function documentation after the name of the function
 
 ;; ### How to call a function?
 (greet "jane")
-
-;; ### How to get documentation of a function?
-;; Documentations are stored as a metadata, so you can get it as:
-(:doc (meta #'greet))
-
-;; But if you are in a REPL you can simply type
-;; ```clojure
-;; (doc greet)
-;; -------------------------------
-;; clj.hello-world/greet
-;; ([name])
-;; Creates a greeting
-;; ```
-;; How does doc work to get all the arguments of a function? As you might have guessed, 
-;; it uses the metadata of the function. And one more technique called macros which will 
-;; cover in a later chapter.
 
 ;; ### How to functional program in Clojure?
 ;; see "cocktail_recipes.clj"
